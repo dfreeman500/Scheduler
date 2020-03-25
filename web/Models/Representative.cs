aff0ce512366d9fdb75c5e84755babf7fbacbac3
Dan <danielfr500@gmail.com>
2020-03-24 21:08:20 -0400

    corrections to class and relationships

--- web/Models/Representative.cs
+++ web/Models/Representative.cs
@@ -2,32 +2,27 @@ using System;
 using System.Collections.Generic;
 namespace web.Models
 {
-    /// <summary>
-    ///One-to-Many
-    ///We need to define the relationship as a collection on one side, and a complex object with an index on the other side.
-    /// </summary>
     public class Base
     {
         public Guid Id { get; set; } //GUID or UUID is an acronym for 'Globally Unique Identifier' or 'Universally Unique Identifier'. It is a 128-bit integer number used to identify resources. 
     }
-    public class Representative : Base // A table of representatives or employees. Each employee will have multiple appointments
-    {
-        public ICollection<Appointment> Appointments { get; set; } //a collection of a appointments
-    }
-    public class Appointment : Base //a table of appointments
-    {
-        public Guid AppointmentId { get; set; }
-        public Client Client { get; set; }
-        public string AppointmentNotes { get; set; } // Ex: put in needs or preferences for that specific appointment
-        public DateTime RequestedTime{get; set;} //I might use an int for this instead. I want to be able to do something like Client A wants an appointment time of 9 and determine who if anyone is available.
-        public DateTime ScheduledTime {get; set;}// This is the actual appointment set by the scheduler onto the representative's schedule
-
-    }
-
-    public class Client //A table of clients
+    // A table of employees. 
+    // Each employee will have multiple appointments
+    public class Employee : Base // Base Generates Employee.Id Guid for identifing an Employee
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int PhoneNumber { get; set; } //from int, can format later if want to ( ) -
+        public ICollection<Appointment> Appointments { get; set; } //a collection of a appointments
+    }
+    // A table of appointments
+    public class Appointment : Base // Base Generates Appointment.Id Guid for identifing an Appointment
+    {
+        // Single use customer
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AppointmentNotes { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime RequestedTime { get; set; }
     }
 }
